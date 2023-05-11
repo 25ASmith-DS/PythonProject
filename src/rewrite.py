d5cc77f7b6e24392c892a36505f774a8c80a3e7b
25ASmith-DS <25asmith@derryfield.org>
2023-05-11 08:01:22 -0400

    scrapped idea

--- src/rewrite.py
+++
@@ -1,8 +0,0 @@
-from option import Option, some, none
-
-
-def list_get(list, index: int) -> Option:
-    if 0 <= index < len(list):
-        return some(list[index])
-    else:
-        return none()
